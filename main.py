88bf4c7149fc0f24e31c4d0a8dfe20186a212e8d
s3794510 <walter.tungngo@gmail.com>
2022-01-06 05:55:46 +0700

    handle sound effect, create 4 threading
    create threading:
    - handle exit when press
    - handle pause when press
    - handle unpause when press
    - handle show fps when press

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ import os,winsound
 from time import sleep, time
 from bothandler import BotHandler
 
-# Change the working directory to the folder this script is in.
-# Doing this because I'll be putting the files from each video in their own folder on GitHub
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+#os.chdir(os.path.dirname(os.path.abspath(__file__)))
 window_name = 'Leaf Blower Revolution'
 #window_name = 'areasxx.jpg - Paint'
-
 # initialize bot
 bot = BotHandler(window_name)
 debug = None
 print("""Hold shift + ESC to stop
-Hold shift + P to pause.
+Hold shift + P to pause/unpause.
 Hold shift + F to show FPS
 Program is running.
 """)
 bot.run()
 print('Program is closed.')
+i = input("Press any key to end program.\n")
 
