5a70a524d11f0b27ce82ee68f9c7958bd9902c16
Tung <62049425+s3794510@users.noreply.github.com>
2022-12-27 06:31:02 +0700

    setup.py changes
    adjusting python version requirement
    adjust module requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 import sys
-if sys.version_info >= (3,7):
-    sys.exit('Sorry, Python > 3.7 is not supported, this is due to the mode pywin32 is not fully support for python 3.7 or above')
+#if sys.version_info >= (3,7):
+#    sys.exit('Sorry, Python > 3.7 is not supported, this is due to the mode pywin32 is not fully support for python 3.7 or above')
 setup()
