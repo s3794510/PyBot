eb4ef92490bd3ddc3d2d5fb22e493e9ae59ee502
Tung <62049425+s3794510@users.noreply.github.com>
2022-12-27 06:29:21 +0700

    Update bothandler.py
    remove pygame import
    add sound functions

--- pybotter/bothandler.py
+++ pybotter/bothandler.py
@@ -4,7 +4,7 @@ from time import time, sleep
 import keyboard, cv2, os
 from .vision import Vision
 from .windowhandler import WindowHandler
-from pygame import mixer
+
 class BotHandler:
     # http://www.kbdedit.com/manual/low_level_vk_list.html
     keymap = {
@@ -130,6 +130,50 @@ class BotHandler:
 
             self.flow_handle(0)
         cv2.destroyAllWindows()
+    
+    def _playsoundWin(self, sound, block = True):
+        """
+        This function is a replica from playsound 1.2.2, kudos to TaylorSMarks.
+        See their GitHub on https://github.com/TaylorSMarks/
+        """
+        '''
+        Utilizes windll.winmm. Tested and known to work with MP3 and WAVE on
+        Windows 7 with Python 2.7. Probably works with more file formats.
+        Probably works on Windows XP thru Windows 10. Probably works with all
+        versions of Python.
+
+        Inspired by (but not copied from) Michael Gundlach <gundlach@gmail.com>'s mp3play:
+        https://github.com/michaelgundlach/mp3play
+
+        I never would have tried using windll.winmm without seeing his code.
+        '''
+        from ctypes import c_buffer, windll
+        from random import random
+        from time   import sleep
+        from sys    import getfilesystemencoding
+
+        def winCommand(*command):
+            buf = c_buffer(255)
+            command = ' '.join(command).encode(getfilesystemencoding())
+            errorCode = int(windll.winmm.mciSendStringA(command, buf, 254, 0))
+            if errorCode:
+                errorBuffer = c_buffer(255)
+                windll.winmm.mciGetErrorStringA(errorCode, errorBuffer, 254)
+                exceptionMessage = ('\n    Error ' + str(errorCode) + ' for command:'
+                                    '\n        ' + command.decode() +
+                                    '\n    ' + errorBuffer.value.decode())
+                raise Exception(exceptionMessage)
+            return buf.value
+
+        alias = 'playsound_' + str(random())
+        winCommand('open "' + sound + '" alias', alias)
+        winCommand('set', alias, 'time format milliseconds')
+        durationInMS = winCommand('status', alias, 'length')
+        winCommand('play', alias, 'from 0 to', durationInMS.decode())
+
+        if block:
+            sleep(float(durationInMS) / 1000.0)
+        
     def pause(self):
         self.is_pause = True
         print("Paused.\n")
@@ -162,11 +206,12 @@ class BotHandler:
             thread.start()
 
     def launch_mp3(self, sound_file):
-            mixer.init()
-            mixer.music.load(sound_file)
-            mixer.music.play()
-            while mixer.music.get_busy():  # wait for music to finish playing
-                sleep(1)
+            #mixer.init()
+            #mixer.music.load(sound_file)
+            #mixer.music.play()
+            #while mixer.music.get_busy():  # wait for music to finish playing
+            #    sleep(1)
+            self._playsoundWin(sound_file)
     
     def pause_handle_thread(self):
         thread = threading.Thread(target=self.pause_handle, args=())
