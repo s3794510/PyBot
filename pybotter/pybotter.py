fefd5e68f0906042f8766089d2f55d7b804761ef
Tung <62049425+s3794510@users.noreply.github.com>
2022-12-28 17:38:59 +0700

    fixing for unittesting

--- pybotter/pybotter.py
+++ pybotter/pybotter.py
@@ -12,16 +12,16 @@ class PyBot:
         return WindowHandler.list_window_titles()
 
     def add_image(self, name, path):
-        self.bothandler.add_image(name, path)
+        return self.bothandler.add_image(name, path)
 
     def find_image(self, name, threshold = 0.5):
-        self.bothandler.find_image(name, threshold)
+        return self.bothandler.find_image(name, threshold)
 
     def show_window(self):
         self.bothandler.show_screenshot()
 
 
-    def run(self, debug = None):
+    def run(self):
 
         # Start program text
         print("""Hold shift + ESC to stop
@@ -31,18 +31,42 @@ class PyBot:
         """)
 
         # Run the bot
-        self.bothandler.run(debug)
+        self.mainloop()
 
         # After done running
         print('Program is closed.')
         return 0
 
+    def mainloop(self):
+        self.bothandler.init(self.debug)
+        self.add_image("Sample button", "SampleButton.png")
+        while(self.bothandler.is_running):
+    
+            # get an updated image of the game
+            self.bothandler.update_screenshot()
+
+            # debug: pop up a window that show the screen shot
+            #if(self.debug):
+                #self.bothandler.show_screenshot()
+
+            # Put the actions (mouse/keyboard) inside function actions in this class
+            self.actions()
+
+            self.bothandler.flow_handle()
+        self.bothandler.destroyAllWindows()
+
 
     def left_click(self, x, y, duration):
         self.bothandler.leftclick(x, y, duration)
 
+    def key_press(self, key, duration):
+        return self.bothandler.keyboard_press(key, duration)
+
     def resize(self, x, y):
-        self.bothandler.resize(x, y)
+        return self.bothandler.resize(x, y)
         
 
-    
+    def actions(self):
+        # Put the actions here
+        self.find_image("Sample button",0.5)
+        pass
